3cb9094be5e766dd093f14a1602f5f9b46571352
Leandro Alonso <leandroalonso_@hotmail.com>
2024-12-09 15:11:15 -0300

    fargate

--- day-3-ecs-service-module/ecs-service.tf
+++ day-3-ecs-service-module/ecs-service.tf
@@ -18,9 +18,12 @@ resource "aws_ecs_service" "service-01" {
     rollback = true
   }
 
-  ordered_placement_strategy {
-    type  = "spread"
-    field = "attribute:ecs.availability-zone"
+  dynamic "ordered_placement_strategy" {
+    for_each = var.service_launch_type == "EC2" ? [1] : []
+    content {
+      type  = "spread"
+      field = "attribute:ecs.availability-zone"
+    }
   }
 
   network_configuration {
